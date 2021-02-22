0aa9a6616a8486ff3c2e9f23c1b4f824a270e935
Nietsnie-beep <AL02982380@tecmilenio.mx>
2021-02-22 08:13:43 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import model.*;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //showMenu();
+
+        Doctor myDoctor = new Doctor("Anahí Salgado", "anahi@anahi.com");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
+
+        System.out.println(myDoctor);
+
+        User user = new Doctor("Nietsnie", "Correo@gamil.com");
+        user.showDataUser();
+
+        User userPa = new Patient("(.)y(.)","alguno@gmail.com");
+        userPa.showDataUser();
+
+        //clase anonima
+        User user1 = new User("(.)Y(.)", "algo@") {
+            @Override
+            public void showDataUser() {
+                System.out.println("Doctor");
+                System.out.println("Hospital: Cruz algo");
+                System.out.println("Departamento: Chichis sanas");
+            }
+        };
+
+        user1.showDataUser();
+
+        ISchedulable iSchedulable = new ISchedulable() {
+            @Override
+            public void schedule(Date date, String time) {
+
+            }
+        };
+
+        /*
+
+        for (model.Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
+        */
+
+/*
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        System.out.println(patient);
+*/
+    }
+
+
+
+}
---
+++ src/model/Nurse.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Nurse extends User{
+
+    private String speciality;
+
+    public Nurse(String name, String email) {
+        super(name, email);
+    }
+
+    @Override
+    public void showDataUser() {
+        System.out.println("Hospital Cruz verde");
+        System.out.println("Departamento Nutriologia");
+
+    }
+
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
